47dcbda4f3be6bd95c8558f3932ef8bb22f8d89f
Zara Barbazova <barbazovasvetka@outlook.com>
2022-06-12 08:00:30 +0300

    Add project files.

---
+++ WebDriverTestDEmo/WikipediaTest.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+var driver = new ChromeDriver();
+
+driver.Url = "https://wikipedia.org";
+
+System.Console.WriteLine("Curent Titel" + driver.Title);
+var searchField = driver.FindElement(By.Id("searchInput"));
+searchField.Click();
+searchField.SendKeys("Quality Assurance" + Keys.Enter);
+System.Console.WriteLine("Title after search" + driver.Title);
+
+driver.Quit();
